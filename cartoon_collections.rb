c39c40d1eebda04a86ae87a2dba1fc3f58bfce06
Rafael Cruz <ralph1786@gmail.com>
2018-11-23 18:35:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(long_calls)
 end
 
 def find_the_cheese(chance_of_cheese)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   chance_of_cheese.find { |perhaps_cheese|
     cheese_types.include?(perhaps_cheese)
