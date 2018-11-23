d9c00084aa38edc0897c11f1c4f66567c7c456b2
Rafael Cruz <ralph1786@gmail.com>
2018-11-23 18:34:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,7 @@ end
 def find_the_cheese(chance_of_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  chance_of_cheese.find { |perhaps_cheese|
+    
+  }
 end
